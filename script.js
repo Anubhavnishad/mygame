284139fab9d61324106c365dfe638c7d6bdb4977
Anubhav Nishad <147842558+Anubhavnishad@users.noreply.github.com>
2025-11-10 23:40:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let move_speed = 3, gravity = 0.5;
-let bird = document.querySelector('.bird');
-let img = document.getElementById('bird-1');
+let bird = document.querySelector('./.bird');
+let img = document.getElementById('./bird-1');
 
 let sound_point = new Audio('sounds_effect/point.mp3');
 let sound_die = new Audio('sounds_effect/die.mp3');
